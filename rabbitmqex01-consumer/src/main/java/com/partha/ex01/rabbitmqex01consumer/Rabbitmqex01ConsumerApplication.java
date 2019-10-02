4ed5d86f9f8db285acfe08c37473e15641352edf
biswaspa <biswaspa@biswaspa-z01>
2019-10-03 02:18:41 +0530

    example01

---
+++ rabbitmqex01-consumer/src/main/java/com/partha/ex01/rabbitmqex01consumer/Rabbitmqex01ConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.partha.ex01.rabbitmqex01consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rabbitmqex01ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rabbitmqex01ConsumerApplication.class, args);
+	}
+
+}
